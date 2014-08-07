9dcc86a038fb157f77d79163303f1e1f40aefa92
Victor Miranda <victor@vpc-2.local>
2014-08-07 19:04:00 +0200

    #34 Improve autocomplete

--- docroot/js/sql-autocomplete.js
+++ docroot/js/sql-autocomplete.js
@@ -3,6 +3,7 @@ YUI.add('sql-autocomplete', function (Y, NAME) {
 	var Lang = Y.Lang,
 		DOT = '.',
 		STR_EMPTY = '',
+		SQL_WORDS = ['SELECT','FROM','WHERE','INSERT','UPDATE','DELETE','JOIN','AND','OR','ALTER','UNION','DISTINCT','COUNT','NULL','NOT'],
 
 		NAME = 'sql-autocomplete';
 
@@ -37,20 +38,21 @@ YUI.add('sql-autocomplete', function (Y, NAME) {
 			getMatch: function(content) {
 
 				var instance = this;
+
 				var match;
 
 				var matchIndex;
 
-				if ((matchIndex = content.lastIndexOf(DOT)) >= 0) {
-					//FIXME, it has to be the last whitespace UNTIL THE CURSOR
-					var lastWhiteSpace = content.lastIndexOf(' ');
-					content = content.substring(lastWhiteSpace+1);
+				var words = content.split(/[,\/ -]/);
 
-					match = {
-						content: content,
-						start: matchIndex
-					};
-				}
+				content = words[words.length -1];
+
+				console.log('getMatch ' + content);
+
+				match = {
+					content: content,
+					start: matchIndex
+				};
 
 				return match;
 			},
@@ -60,7 +62,11 @@ YUI.add('sql-autocomplete', function (Y, NAME) {
 
 				var type = match.type;
 
-				var matches = instance._getVariableMatches(match.content);
+				var tableMatches = instance._getTableMatches(match.content) || [];
+				var keywordMatches = instance._getKeywordMatches(match.content) || [];
+				var fieldMatches = instance._getFieldMatches(match.content) || [];
+
+				var matches = keywordMatches.concat(tableMatches.concat(fieldMatches));
 
 				callbackSuccess(matches);
 			},
@@ -70,42 +76,143 @@ YUI.add('sql-autocomplete', function (Y, NAME) {
 
 				var result = selectedSuggestion || STR_EMPTY;
 
-				var contentParts = match.content.split(DOT);
-				var fieldName = contentParts[contentParts.length-1];
+				var contentParts = match.content.trim().split(DOT);
+				var autocompleting = contentParts[contentParts.length-1];
 
 				if (selectedSuggestion) {
-					if (fieldName && selectedSuggestion.indexOf(fieldName) === 0) {
+					if (autocompleting && selectedSuggestion.toLowerCase().indexOf(autocompleting.toLowerCase()) === 0) {
+						result = selectedSuggestion.substring(autocompleting.length);
 
-						result = selectedSuggestion.substring(fieldName.length);
+						var lastLetter = match.content.charAt(match.content.length-1);
+
+						if (lastLetter !== lastLetter.toUpperCase()) {
+							result = result.toLowerCase();
+						}
 					}
 				}
 
 				return match.content+result;
 			},
 
-
-			_getVariableMatches: function(content) {
+			_getKeywordMatches: function(content) {
 				var instance = this;
-
-				var contentParts = content.split(DOT);
 
 				var matches = [];
 
-				var tables = instance.get('schema');
+				var contentToSQLWords = content.trim();
 
-				for (var i in tables) {
-					if (tables[i].table.toLowerCase() == contentParts[0].toLowerCase()) {
-						for (var j in tables[i].fields) {
-							var f = tables[i].fields[j];
-							if (f.label.toLowerCase().indexOf(contentParts[1].toLowerCase())==0) {
-								matches.push(f.label);
-							}
+				if (contentToSQLWords.length > 0) {
+					for (var i in SQL_WORDS) {
+						if (SQL_WORDS[i].toLowerCase().indexOf(contentToSQLWords.toLowerCase()) == 0) {
+							matches.push(SQL_WORDS[i]);
 						}
 					}
 				}
 
 				return matches;
+			},
+
+			_getFieldMatches: function(content) {
+				var instance = this;
+
+				var matches = [];
+
+				var contentParts = content.split(DOT);
+
+				if (contentParts.length == 2) {
+					var editorContent =
+						instance.get('host').get('host').editor.session.getValue();
+
+					var resTables = instance._getTablesWithAliases(
+						contentParts[0].trim(), editorContent);
+
+						for (var i in resTables) {
+							for (var j in resTables[i].fields) {
+								var f = resTables[i].fields[j];
+								if (f.label.toLowerCase().indexOf(
+										contentParts[1].toLowerCase()) == 0) {
+									matches.push(f.label);
+								}
+							}
+						}
+				}
+
+				return matches;
+			},
+
+			_getTableMatches: function(content) {
+				var instance = this;
+
+				if (!content || content.lengt === 0) {
+					return;
+				}
+
+				var matches = [];
+
+				var tables = instance.get('schema');
+
+				content = content.trim();
+
+				for (var i = 0 in tables) {
+					if (tables[i].table.toLowerCase().indexOf(content.toLowerCase()) === 0) {
+						matches.push(tables[i].table);
+					}
+				}
+
+				return matches;
+			},
+
+			_getTablesWithAliases: function(str, editorContent) {
+				var instance = this;
+
+				var res = [];
+
+				if (str === undefined || str.length === 0) {
+					return;
+				}
+
+				str = str.trim();
+
+				var tables = instance.get('schema');
+
+				var tablesWithAliases = tables;
+
+				var alias = instance._getAliases(str, editorContent);
+
+				if (alias) {
+					str = alias;
+				}
+
+				for(var i in tables) {
+					if (tables[i].table.toLowerCase() === str.toLowerCase()) {
+						res.push(tables[i]);
+					}
+				}
+
+				return res;
+			},
+
+			_getAliases: function(alias, sql) {
+				var instance = this;
+
+				var words = sql.split(/[, \t\n]/);
+
+				var tables = instance.get('schema');
+
+				for (var i = 2 in words) {
+					if (words[i] === alias) {
+						//check if previous word is a table
+						for (var j in tables) {
+							if (tables[j].table.toLowerCase() === words[i-1].toLowerCase()) {
+								return tables[j].table;
+							}
+						}
+					}
+				}
+
+				return;
 			}
+
 		}
 	});
 
